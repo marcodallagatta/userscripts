e3999708f585a6778148d32564fd8243ddc76308
marcodallagatta <36917918+marcodallagatta@users.noreply.github.com>
2022-07-13 14:31:36 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# usersccript-time-spent-youtube
+# Time Spent on YouTube Userscript
+
+A friend asked me to make a simple userscript to show how long she spends on YouTube every day.
+I decided to publish it here since it could easily be adapted to other websites without changing the logic.
+
+The script refreshes every 15 seconds only when the YouTube tab is actually active. To make it count time even in background just change the `background` variable to `true`.
+
+## Installation
+
+1. Make sure you have an extension that supports userscripts:
+
+   - Firefox - install [Greasemonkey](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/).
+   - Chrome - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=chrome).
+   - Edge - install [Tampermonkey](https://www.tampermonkey.net/?ext=dhdg&browser=edge).
+   - Opera - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=opera) or [Violent Monkey](https://addons.opera.com/en/extensions/details/violent-monkey/).
+   - Safari - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=safari).
+   - Dolphin - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=dolphin).
+   - UC Browser - install [Tampermonkey](https://tampermonkey.net/?ext=dhdg&browser=ucweb).
+
+2. Install by clicking on the following link: [time-spent-on-youtube](https://tampermonkey.net/?ext=dhdg&browser=ucweb)
+
+## Updating
+
+Userscripts are set up to automatically update. You can check for updates from within the Greasemonkey or Tampermonkey menu, or click on the install link again to get the update.
+
+## Issues
+
+Please report any userscript issues within this repository's [issue section](https://github.com/marcodallagatta/userscript-time-spent-youtube/issues).
---
+++ time-spent-on-youtube.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         Time Spent on Youtube
+// @namespace    https://github.com/marcodallagatta/userscript-time-spent-youtube
+// @version      1.0
+// @description  A simple timer that shows how much time you spent on Youtube today (resets at midnight).
+// @author       @marcodallagatta
+// @updateURL    https://github.com/marcodallagatta/userscript-time-spent-youtube
+// @downloadURL  https://github.com/marcodallagatta/userscript-time-spent-youtube
+// @match        https://www.youtube.com/*
+// @icon         https://icons.duckduckgo.com/ip2/youtube.com.ico
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  const background = false; // 'true' also counts when YT is in a background tab, 'false' only counts active tab
+
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0");
+  const yyyy = today.getFullYear();
+  const todayYYMMDD = yyyy + mm + dd;
+
+  let spent = 0;
+
+  if (localStorage.getItem("date") !== todayYYMMDD) {
+    localStorage.setItem("date", todayYYMMDD);
+    localStorage.setItem("spent", 0);
+  } else {
+    spent = parseFloat(localStorage.getItem("spent"));
+  }
+
+  const inDOM = document.getElementById("start");
+  let elem = document.createElement("span");
+  elem.style = "color:white;font-size:2rem";
+  inDOM.appendChild(elem);
+  showAlert(spent);
+
+  function showAlert(time) {
+    if (time > 60) {
+      elem.innerText = `${Math.trunc(time / 60).toString()}h ${(
+        Math.trunc(time) % 60
+      ).toString()}m`;
+    } else {
+      elem.innerText = `${Math.trunc(time).toString()}m`;
+    }
+  }
+
+  setInterval(function () {
+    if (background || document.hasFocus()) {
+      spent += 0.25;
+      localStorage.setItem("spent", spent);
+      showAlert(spent);
+    }
+  }, 15000); // 15s
+})();
