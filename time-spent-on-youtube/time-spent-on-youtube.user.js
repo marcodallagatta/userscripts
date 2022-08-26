0f52ee18a7646c54534e47a2f8a0dc894d56996a
Marco Dalla Gatta <36917918+marcodallagatta@users.noreply.github.com>
2022-08-26 15:48:49 +0200

    add ability to specify reset time

--- time-spent-on-youtube/time-spent-on-youtube.user.js
+++ time-spent-on-youtube/time-spent-on-youtube.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Time Spent on Youtube
-// @version      2022.09.26.15.43
+// @version      2022.09.26.15.48
 // @description  A simple timer that shows how much time you spent on Youtube today. Visit the GitHub repo for more info: https://github.com/marcodallagatta/userscripts/raw/main/time-spent-on-youtube/
 // @license      MIT
 // @author       Marco Dalla Gatta
@@ -18,17 +18,21 @@
   const background = false; // 'true' also counts when YT is in a background tab, 'false' only counts active tab
   const showInTitle = true; // 'true' prepend the minutes before the title in the tab
   const originalTitle = document.title;
+  const resetTime = 4; // time when the timer resets, 24h time (0 = midnight, 4 = 4AM, 15 = 3PM)
 
   const today = new Date();
   const dd = String(today.getDate()).padStart(2, "0");
   const mm = String(today.getMonth() + 1).padStart(2, "0");
   const yyyy = today.getFullYear();
   const todayYYMMDD = yyyy + mm + dd;
+  const hours = today.getHours();
+
 
   let spent = 0;
 
-  if (localStorage.getItem("date") !== todayYYMMDD) {
+  if (localStorage.getItem("date") !== todayYYMMDD && localStorage.getItem("hours") >= resetTime) {
     localStorage.setItem("date", todayYYMMDD);
+    localStorage.setItem("hours", hours);
     localStorage.setItem("spent", 0);
   } else {
     spent = parseFloat(localStorage.getItem("spent"));
