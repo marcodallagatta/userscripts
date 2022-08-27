4d50b6f0f50620c1a7e1cd1676303d13ce6bd6c1
Marco Dalla Gatta <36917918+marcodallagatta@users.noreply.github.com>
2022-08-27 16:17:39 +0200

    add license

--- youtube-shorts-to-normal/youtube-shorts-to-normal.user.js
+++ youtube-shorts-to-normal/youtube-shorts-to-normal.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         Youtube Shorts Custom Page to Normal Video Page
 // @namespace    https://github.com/marcodallagatta/userscripts/raw/main/youtube-shorts-to-normal
-// @version      2022.08.27.16.12
+// @version      2022.08.27.16.17
 // @description  A simple redirect scripts to change the terrible shorts interface to the one for 'normal' videos
 // @updateURL    https://github.com/marcodallagatta/userscripts/raw/main/youtube-shorts-to-normal/youtube-shorts-to-normal.user.js
 // @downloadURL  https://github.com/marcodallagatta/userscripts/raw/main/youtube-shorts-to-normal/youtube-shorts-to-normal.user.js
 // @author       Marco Dalla Gatta
+// @license      MIT
 // @match        https://www.youtube.com/*
 // @icon         https://icons.duckduckgo.com/ip2/youtube.com.ico
 // @grant        none
