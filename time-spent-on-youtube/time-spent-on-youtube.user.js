5886f28252510eefbb1005c86d33a79cf0b62ddc
Marco Dalla Gatta <36917918+marcodallagatta@users.noreply.github.com>
2022-08-29 12:20:03 +0200

    restore @updateURL

--- time-spent-on-youtube/time-spent-on-youtube.user.js
+++ time-spent-on-youtube/time-spent-on-youtube.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Time Spent on Youtube
-// @version      2022.08.27.16.21
+// @version      2022.08.28.12.19
 // @description  A simple timer that shows how much time you spent on Youtube today.
 // @license      MIT
 // @author       Marco Dalla Gatta
 // @namespace    https://github.com/marcodallagatta/userscripts/raw/main/time-spent-on-youtube/
+// @updateURL    https://github.com/marcodallagatta/userscripts/raw/main/time-spent-on-youtube/time-spent-on-youtube.user.js
+// @downloadURL  https://github.com/marcodallagatta/userscripts/raw/main/time-spent-on-youtube/time-spent-on-youtube.user.js
 // @match        https://www.youtube.com/*
 // @icon         https://icons.duckduckgo.com/ip2/youtube.com.ico
 // ==/UserScript==
