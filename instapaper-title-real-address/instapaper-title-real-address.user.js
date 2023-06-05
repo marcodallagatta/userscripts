4f65ef10c36578f3483692ae88894db2bd1d7bad
Marco Dalla Gatta <36917918+marcodallagatta@users.noreply.github.com>
2023-06-05 09:54:38 +0200

    Create instapaper-title-real-address.user.js

---
+++ instapaper-title-real-address/instapaper-title-real-address.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Instapaper title links to real address
+// @namespace    https://github.com/marcodallagatta/userscripts/tree/main/instapaper-title-real-address
+// @version      1.0
+// @description  Instead of linking to Instapaper's "Reading Mode", this will use the real address' link for the title url
+// @updateURL    https://github.com/marcodallagatta/userscripts/tree/main/instapaper-title-real-address/instapaper-title-real-address.user.js
+// @downloadURL  https://github.com/marcodallagatta/userscripts/tree/main/instapaper-title-real-address/instapaper-title-real-address.user.js
+// @author       You
+// @match        https://www.instapaper.com/u
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=instapaper.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const articles = document.querySelectorAll('article');
+
+    articles.forEach(art => {
+        const url = art.querySelector('.js_bookmark_edit').dataset.url;
+        art.querySelector('.article_title').href = url;
+    })
+})();
