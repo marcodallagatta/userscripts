45a787f9c72ff948b0840fe7c2f5d81ef7433146
Marco Dalla Gatta <36917918+marcodallagatta@users.noreply.github.com>
2023-06-05 10:37:05 +0200

    Update instapaper-title-real-address.user.js
    allows for other urls on instapaper

--- instapaper-title-real-address/instapaper-title-real-address.user.js
+++ instapaper-title-real-address/instapaper-title-real-address.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Instapaper title links to real address
 // @namespace    https://github.com/marcodallagatta/userscripts/tree/main/instapaper-title-real-address
-// @version      1.0
+// @version      1.1
 // @description  Instead of linking to Instapaper's "Reading Mode", this will use the real address' link for the title url
 // @updateURL    https://github.com/marcodallagatta/userscripts/tree/main/instapaper-title-real-address/instapaper-title-real-address.user.js
 // @downloadURL  https://github.com/marcodallagatta/userscripts/tree/main/instapaper-title-real-address/instapaper-title-real-address.user.js
 // @author       You
-// @match        https://www.instapaper.com/u
+// @match        https://www.instapaper.com/u/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=instapaper.com
 // @grant        none
 // ==/UserScript==
